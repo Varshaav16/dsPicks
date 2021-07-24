64c4f83825a8bd265e4ae4bd1e3f25ec7737a680
ravisha7feb <64865136+ravisha7feb@users.noreply.github.com>
2021-07-24 20:14:20 +0530

    add webpage to pdf generator using pdfkit

---
+++ scrape.py
@@ -0,0 +1,16 @@
+import os
+import pdfkit
+
+path_toModule = "C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe"
+config = pdfkit.configuration(wkhtmltopdf=path_toModule) 
+#options = {'allow': 'D:/MY_FILES/hacky-mlh-project/scrapers/pythonScraper/test.py'}
+
+pdfkit.from_url('https://en.wikipedia.org/wiki/Machine_learning', r'.\exports\out.pdf', configuration=config)
+
+path = 'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'
+  
+# Check whether the specified
+# path exists or not
+isExist = os.path.exists(path)
+print(isExist)
+#/mnt/d/MY_FILES/hacky-mlh-project/scrapers/pythonScraper
