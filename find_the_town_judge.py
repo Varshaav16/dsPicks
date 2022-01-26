e01fd7db5295b1cf9a4018cb7128740f8a46a027
Ravisha Sharma <ravisha7feb@gmail.com>
2022-01-27 00:15:39 +0530

    add soln to town judge ques and fixed unit test file

--- find_the_town_judge.py
+++ find_the_town_judge.py
@@ -1,4 +1,4 @@
-def findJudge(n, trust):
+def find_judge(n, trust):
         degree = [0] * (n + 1)
         for truster, trustee in trust:
             degree[truster]-= 1
@@ -7,5 +7,5 @@ def findJudge(n, trust):
         for i in range(1, n + 1):
             if degree[i] == (n - 1):
                 return i
-        return -1;
+        return -1
         
--- test_find_the_town_judge.py
+++ test_find_the_town_judge.py
@@ -5,7 +5,7 @@ class TestSolution(unittest.TestCase):
     def test_find_the_town_judge(self):
         self.assertEqual(find_the_town_judge.find_judge(2, [[1,2]]), 2)  
         self.assertEqual(find_the_town_judge.find_judge(3, [[1,3],[2,3],[3,1]]), -1)  
-        self.assertEqual(find_the_town_judge.find_judge(2, [[1,2], [2, 1]]), 2)  
+        self.assertEqual(find_the_town_judge.find_judge(2, [[1,2], [2, 1]]), -1)  
         self.assertEqual(find_the_town_judge.find_judge(3, [[1,3],[2,3]]), 3)  
 
 
