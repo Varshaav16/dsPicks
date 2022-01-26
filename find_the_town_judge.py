5717b394a1e4c03b40ecebadbb3d53d95b454bfb
ravisha7feb <ravisha7feb@gmail.com>
2022-01-27 00:06:34 +0530

    add soln to town judge ques

---
+++ find_the_town_judge.py
@@ -0,0 +1,11 @@
+def findJudge(n, trust):
+        degree = [0] * (n + 1)
+        for truster, trustee in trust:
+            degree[truster]-= 1
+            degree[trustee]+= 1
+            
+        for i in range(1, n + 1):
+            if degree[i] == (n - 1):
+                return i
+        return -1;
+        
